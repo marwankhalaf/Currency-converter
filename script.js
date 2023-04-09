1b1cf5fd58ea3adc20eea2f3c7e5036015140997
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2023-04-09 03:33:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// start logic
+
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const fromDropDown = document.getElementById("from-currency-select");
 const toDropDown = document.getElementById("to-currency-select");
